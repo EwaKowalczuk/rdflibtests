7d915553bdb13923f99d79f0d4b1c4fa1872552e
EwaKowalczuk <nuoritoveri@gmail.com>
2015-10-03 23:35:09 +0200

    Issuing different queries.

--- checkLubmAnswers.py
+++ checkLubmAnswers.py
@@ -2,13 +2,13 @@
 import os
 import re
 import sys
+import subprocess
 
 #check if query result matches answer
 
 if(len(sys.argv) != 4):
     exit("python3 checkLubmAnswers.py <queryDirectory> <answerDirectory> <sparqlAddress>")
 
-#TODO use SPARQL instead of isql
         
 queryDirectory = sys.argv[1]
 answerDirectory = sys.argv[2]
@@ -18,8 +18,24 @@ sparqlAddress = sys.argv[3]
 QUERY_FILE_PREFIX = 'query'
 ANSWER_FILE_PREFIX = 'answers_query'
 
-for f in os.listdir(queryDirectory):
-    print(f)
+#TODO add inference!
+        
+for queryFileName in os.listdir(queryDirectory):
+    fullQueryFileName = os.path.join(queryDirectory, queryFileName)
+    #issue query
+    #TODO use SPARQL instead of isql
+    #TODO use rdflib instead of sparel
+    with open(fullQueryFileName) as queryFile:
+        query = [line.rstrip('\n') for line in queryFile.readlines() if not line.startswith('#')]
+        query.insert(0, "SPARQL")
+        query.append(";")
+        queryString = "\n".join(query)
+        #TODO add SPARQL + ;
+        proc = subprocess.Popen(["/home/nuoritoveri/install/virtuoso/virtuoso/bin/isql" , "1111", "dba", "dba"], 
+                stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.PIPE, universal_newlines = True)
+        stdout, stderr = proc.communicate(queryString)
+        print()
+        print(stdout)
     #m = re.match('uba1.7\\\\(University.+.owl)', f)
     #if m:
     #    newname = m.groups()[0]
