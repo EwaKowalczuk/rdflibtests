87e96297b5f209056bb2097fb3a444387e1dacaa
EwaKowalczuk <nuoritoveri@gmail.com>
2015-10-04 12:59:16 +0200

    Can has pragmas - test.

--- checkLubmAnswers.py
+++ checkLubmAnswers.py
@@ -40,6 +40,7 @@ ANSWER_FILE_PREFIX = 'answers_query'
 #from SPARQLWrapper import SPARQLWrapper, JSON
 
 query = """
+DEFINE  input:inference  'lubmruleset'
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 PREFIX ub: <http://www.lehigh.edu/~zhp2/2004/0401/univ-bench.owl#>
 SELECT ?X	
